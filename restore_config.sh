a9273604c7d4079c88b1ee8162fdca1fc878b3f1
FabHaar <fabienhaar@gmail.com>
2023-11-22 10:30:44 +0100

    update commentaire restore_config.sh

--- restore_config.sh
+++ restore_config.sh
@@ -2,3 +2,6 @@
 
 #Application de l'ancienne config directement dans la base de données
 mysql -u root -p'foo' -h 127.0.0.1 dolibarr < sql/save.sql
+
+#le fichier save .sql a été récupéré avec la commande suivante : 
+#mysqldump -u root -p'foo' -h 127.0.0.1 dolibarr > sql/save.sql
