868e1dddddec11498db69b9cdfbb1df6bb65b1dc
FabHaar <fabienhaar@gmail.com>
2023-11-29 14:12:34 +0100

    update dockerfile

--- purge.sh
+++ purge.sh
@@ -4,7 +4,10 @@ mysql -u dolibarr -p'dolibarr' -h 127.0.0.1 --port=3306 dolibarr < "sql/purge.sq
 
 docker stop SQL_Server
 docker stop Dolibarr_CRM
+docker stop cron_backup
 
 docker rm SQL_Server
 docker rm Dolibarr_CRM
+docker rm cron_backup
+
 docker network rm sae51
