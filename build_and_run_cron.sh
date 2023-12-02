afd3882ab6f5195416a0f7ab1956a8d24d0955e3
FabHaar <fabienhaar@gmail.com>
2023-12-02 12:24:30 +0100

    crontab fonctionnel

--- build_and_run_cron.sh
+++ build_and_run_cron.sh
@@ -5,6 +5,7 @@ docker build -t cron_save -f cron/Dockerfile .
 docker run -d \
 	--name cron_backup \
 	-v dolibarr_docs:/var/www/documents \
+	--network=sae51 \
 	cron_save
 
-#--network=sae51 \
+
--- cron/save_hebdo.sh
+++ cron/save_hebdo.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-mysqldump -u root -p'foo' -h 127.0.0.1 --port=3306 dolibarr > /var/www/documents/save.sql
+mysqldump -u root -p'foo' -h SQL_Server --port=3306 dolibarr > /var/www/documents/save.sql
 
 #mysqldump -u cron -p'cron' -h 127.0.0.1 --port=3306 dolibarr > /var/www/documents/save.sql
 #il faudra rendre active la ligne commenté au lieu de faire avec root
