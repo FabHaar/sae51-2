f496ae3481c11166a4bd4804def261b458af0f7b
FabHaar <fabienhaar@gmail.com>
2023-12-04 12:04:01 +0100

    derniere save lundi matin

--- import_csv.sh
+++ import_csv.sh
@@ -2,7 +2,8 @@
 
 mysql -u root -p'foo' -h 127.0.0.1 --port=3306 dolibarr<< EOF
 LOAD DATA INFILE 'csv/exampleexport.csv'
+INTO TABLE llx_societe
 FIELDS TERMINATED BY ','
-LINE TERMINATED BY '\n'
+LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 EOF
