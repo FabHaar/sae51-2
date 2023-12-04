ad515bc4b823f0a7088556efdf35c7057a31752c
FabHaar <fabienhaar@gmail.com>
2023-12-04 17:58:03 +0100

    save fin

--- import_csv.sh
+++ import_csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tail -n +2 "csv/example_export.csv" | while IFS="," read -r _ Name Alias_Name _ Status Customer Vendor Creation_date Latest_modification_date Customer_Code _ _ _ Address Zip_Code City State_Province Region Country Country_code Phone Fax Url Email _ _ Professional_ID_1 Professional_ID_2 Professional_ID_3 Professional_ID_4 Professional_ID_5 _ VAT_ID Capital _ _ Third_party_type _ Business_entity_type _ Prospect_status _;
+tail -n +2 "csv/example_export.csv" | while IFS="," read -r _ Name Alias_Name _ Status Customer Vendor Creation_date Customer_Code _ _ _ Address Zip_Code City State_Province Region Country Country_code Phone Fax Url Email _ _ Professional_ID_4 Professional_ID_5 _ VAT_ID Capital _ _ Third_party_type _ Business_entity_type _ Prospect_status _;
 do
 	mysql -u root -p'foo' -h 127.0.0.1 --port=3306 dolibarr < sql/third_parts.sql
 done
--- sql/third_parts.sql
+++ sql/third_parts.sql
@@ -1,11 +1,11 @@
-INSERT INTO llx_societe (Name, Alias Name, Status, Customer, Vendor, Creation date, 
-Latest modification date, Customer Code, Address, Zip Code, City, State/Province, 
-Region, Country, Country code, Phone, Fax, Url, Email, 
-Professional ID 1, Professional ID 2, Professional ID 3, Professional ID 4, Professional ID 5, 
+INSERT INTO llx_societe (nom, name_alias, status, client, fournisseur, datec, 
+code_client, address, zip, town,
+Region, Country, Country code, phone, fax, url, email, 
+Professional ID 4, Professional ID 5, 
 VAT ID, Capital, Third party type, Business entity type, Prospect status)
 
 VALUES ('$Name', '$Alias_Name', '$Status', '$Customer', '$Vendor', '$Creation_date', 
-'$Latest_modification date', '$Customer_Code', '$Address', '$Zip_Code', '$City', '$State_Province', 
+'$Customer_Code', '$Address', '$Zip_Code', '$City', 
 '$Region', '$Country', '$Country_code', '$Phone', '$Fax', '$Url', '$Email', 
-'$Professional_ID_1', '$Professional_ID_2', '$Professional_ID_3', '$Professional_ID_4', '$Professional_ID_5', 
+'$Professional_ID_4', '$Professional_ID_5', 
 '$VAT_ID', '$Capital', '$Third_party_type', '$Business_entity_type', '$Prospect_status');
