4b6233c2ce7408ff9c7e7f60cb5fcc359df6dbae
FabHaar <fabienhaar@gmail.com>
2023-12-04 15:26:11 +0100

    test insert csv

--- import_csv.sh
+++ import_csv.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mysql -u root -p'foo' -h 127.0.0.1 --port=3306 dolibarr<< EOF
-LOAD DATA INFILE 'csv/exampleexport.csv'
-INTO TABLE llx_societe
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-EOF
+tail -n +2 "csv/example_export.csv" | while IFS="," read -r nom prenom;
+do
+	mysql -u root -p'foo' -h 127.0.0.1 --port=3306 dolibarr < sql/third_parts.sql
+done
