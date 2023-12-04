685eea9cd8640ede57c0e32a50b8b345bf954519
FabHaar <fabienhaar@gmail.com>
2023-12-04 17:28:37 +0100

    à reprendre

--- sql/third_parts.sql
+++ sql/third_parts.sql
@@ -1,11 +1,11 @@
-INSERT INTO llx_societe ('Name', 'Alias Name', 'Status', 'Customer', 'Vendor', 'Creation date', 
-'Latest modification date', 'Customer Code', 'Address', 'Zip Code', 'City', 'State/Province', 
-'Region', 'Country', 'Country code', 'Phone', 'Fax', 'Url', 'Email', 
-'Professional ID 1', 'Professional ID 2', 'Professional ID 3', 'Professional ID 4', 'Professional ID 5', 
-'VAT ID', 'Capital', 'Third party type', 'Business entity type', 'Prospect status')
+INSERT INTO llx_societe ('Name', Alias Name, "Status", Customer, Vendor, Creation date, 
+Latest modification date, Customer Code, Address, Zip Code, City, State/Province, 
+Region, Country, Country code, Phone, Fax, Url, Email, 
+Professional ID 1, Professional ID 2, Professional ID 3, Professional ID 4, Professional ID 5, 
+VAT ID, Capital, Third party type, Business entity type, Prospect status)
 
 VALUES ('$Name', '$Alias_Name', '$Status', '$Customer', '$Vendor', '$Creation_date', 
-'$Latest_modification date', '$Customer_Code', '$Address', '$Zip_Code', '$City', '$State/Province', 
+'$Latest_modification date', '$Customer_Code', '$Address', '$Zip_Code', '$City', '$State_Province', 
 '$Region', '$Country', '$Country_code', '$Phone', '$Fax', '$Url', '$Email', 
 '$Professional_ID_1', '$Professional_ID_2', '$Professional_ID_3', '$Professional_ID_4', '$Professional_ID_5', 
 '$VAT_ID', '$Capital', '$Third_party_type', '$Business_entity_type', '$Prospect_status');
