0c2487b552e94d62f1c762b7d1fa92ba033c337f
FabHaar <fabienhaar@gmail.com>
2023-12-05 16:54:59 +0100

    tentative plus simple csv

--- import_csv.sh
+++ import_csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tail -n +2 "csv/example_export.csv" | while IFS="," read -r _ Name Alias_Name _ Status Customer Vendor Creation_date Customer_Code _ _ _ Address Zip_Code City State_Province Region Country Country_code Phone Fax Url Email _ _ Professional_ID_4 Professional_ID_5 _ VAT_ID Capital _ _ Third_party_type _ Business_entity_type _ Prospect_status _;
+tail -n +2 "csv/export_simple.csv" | while IFS="," read -r name alias_name;
 do
 	mysql -u root -p'foo' -h 127.0.0.1 --port=3306 dolibarr < sql/third_parts.sql
 done
--- sql/third_parts.sql
+++ sql/third_parts.sql
@@ -1,11 +1,3 @@
-INSERT INTO llx_societe (nom, name_alias, status, client, fournisseur, datec, 
-code_client, address, zip, town,
-Region, Country, Country code, phone, fax, url, email, 
-Professional ID 4, Professional ID 5, 
-VAT ID, Capital, Third party type, Business entity type, Prospect status)
+INSERT INTO llx_societe (nom, name_alias)
 
-VALUES ('$Name', '$Alias_Name', '$Status', '$Customer', '$Vendor', '$Creation_date', 
-'$Customer_Code', '$Address', '$Zip_Code', '$City', 
-'$Region', '$Country', '$Country_code', '$Phone', '$Fax', '$Url', '$Email', 
-'$Professional_ID_4', '$Professional_ID_5', 
-'$VAT_ID', '$Capital', '$Third_party_type', '$Business_entity_type', '$Prospect_status');
+VALUES ('$name', '$alias_Name');
